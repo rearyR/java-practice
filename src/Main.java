75f64843cd38f3b0af3280de26363af27ee8b6eb
quack <122710532+rearyR@users.noreply.github.com>
2025-02-20 12:36:26 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void printEvery3(){
+        for (int i = 1; i < 89; i += 3) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        printEvery3();
+    }
+}
